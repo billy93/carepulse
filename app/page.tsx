92ec31df0b70d0de90282cda847b15aa1668cdcc
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-07-08 17:00:23 +0700

    feature: admin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+import  PatientForm  from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/*TODO : OTP */}
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image 
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
@@ -16,11 +21,11 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm/>
+          <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              &copy; 2024 CarePulse
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
@@ -35,7 +40,9 @@ export default function Home() {
         width={1000}
         alt="patient"
         className="side-img max-w-[50%]"
-        />
+      />
     </div>
-  )
-}
+  );
+};
+
+export default Home;
