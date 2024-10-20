6399f01fc7b816b4806b66286a05aeb45f8037c2
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-10-20 22:54:57 +0700

    add force dynamic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,6 +6,7 @@ import { columns } from "@/components/table/columns";
 import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
+export const dynamic = 'force-dynamic';
 const AdminPage = async () => {
   const appointments = await getRecentAppointmentList();
 
